530f680bf4695c1125216433ac24eaf153de5bce
Recio Eliniel <elinielvrecio@iskolarngbayan.pup.edu.ph>
2023-12-07 09:40:38 +0800

    new

--- age.c
+++ age.c
@@ -11,12 +11,12 @@ int main() {
     // Checking eligibility using if-else statement
 	if (age <= 12) {
         // If the age is less than 12, that means you are a child
-        printf("You are a child.");
+        printf("'C'");
     } else if (age >= 13 && age <= 19) {
         // If the age is greater than or equal to 13 and age is less than or equal to 19, you are a teenager
-        printf("You are a teenager.");
+        printf("'T'");
     } else  {
-        printf("You are an adult.");
+        printf("'A'");
     } 
 
 return 0;
